4df8da4d1d6c7121fa13d2e2c6d399109e16b83e
Lauren Kohn <lauren.kohn@soft-adv.com>
2020-09-13 21:57:41 +0000

    Automatically backed up by Learn

--- lib/insert.sql
+++ lib/insert.sql
@@ -1,4 +1,4 @@
-INSERT INTO bears (name, age, gender, color, temperament, alive) VALUES ("Mr. Chocolate", 4, "M", "brown", "relaxed", 1);'
+INSERT INTO bears (name, age, gender, color, temperament, alive) VALUES ("Mr. Chocolate", 4, "M", "brown", "relaxed", 1);
 
 'INSERT INTO bears (name, age, gender, color, temperament, alive) VALUES ("Rowdy", 16, "M", "brown", "relaxed", 1);'
 
